1b32746d1026343370e8b58d2bbf1eed965b1ef6
VeerMuchandi <veer.muchandi@gmail.com>
2015-10-05 20:53:05 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <div style="display:block;margin-left:auto;margin-right:auto;text-align:center;">
+            <h2>Welcome to the world of</h2>
+        </div>
+	<div style="width:400px;height:400px;overflow:hidden;display:block;margin-left:auto;margin-right:auto;">
+            <img src="sonylogobig.jpg" width="400px" height="auto"/>
+        </div>
+    </body>
+</html>
