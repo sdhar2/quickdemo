066840d05afab7d32aac51813d4a42b79704e6bb
VeerMuchandi <veer.muchandi@gmail.com>
2015-10-08 13:37:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
     </head>
     <body>
         <div style="display:block;margin-left:auto;margin-right:auto;text-align:center;">
-            <h2>Welcome to the wonderful world of</h2>
+            <h2>Welcome to the world of</h2>
         </div>
 	<div style="width:400px;height:400px;overflow:hidden;display:block;margin-left:auto;margin-right:auto;">
-            <img src="cboe1.jpeg" width="400px" height="auto"/>
+            <img src="cboe2.jpeg" width="400px" height="auto"/>
         </div>
     </body>
 </html>
