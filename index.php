fd506f9c0a75718e1a405bf6ea64e31334a87f92
VeerMuchandi <veer.muchandi@gmail.com>
2016-03-12 21:48:53 -0500

    small code change

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     </head>
     <body style="background-color:white">
         <div style="display:block;margin-left:auto;margin-right:auto;text-align:center;">
-            <h2>Welcome to </h2>
+            <h2>Welcome to Ravindra </h2>
         </div>
 	<div style="width:400px;height:200px;overflow:hidden;display:block;margin-left:auto;margin-right:auto;">
             <img src="openshift.png" width="400px" height="auto"/>
